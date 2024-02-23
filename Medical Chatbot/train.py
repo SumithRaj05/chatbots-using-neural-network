045b503094561e43ad0d6d0446a50caeea3425ea
SumithRaj05 <sumithrajpurohit03+1@gmail.com>
2024-02-23 22:58:39 +0530

    improved accuracy of medical bot

--- Medical Chatbot/train.py
+++ Medical Chatbot/train.py
@@ -2,11 +2,10 @@ import json
 import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, GlobalAveragePooling1D, Dense
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
-
-model = load_model("model.keras")
 
 # Load the new dataset
 with open('intents.json') as file:
@@ -25,26 +24,30 @@ tokenizer.fit_on_texts(patterns)
 sequences = tokenizer.texts_to_sequences(patterns)
 padded_sequences = pad_sequences(sequences, padding='post')
 
-
 # Encode labels
 label_encoder = LabelEncoder()
 label_encoder.fit(labels)
 encoded_labels = label_encoder.transform(labels)
 categorical_labels = to_categorical(encoded_labels)
 
-# Function to get a response
-def get_response(message):
-    sequence = tokenizer.texts_to_sequences([message])
-    padded = pad_sequences(sequence, maxlen=padded_sequences.shape[1], padding='post')
-    prediction = model.predict(padded)
-    intent = label_encoder.inverse_transform([np.argmax(prediction)])
-    responses = next(item['responses'] for item in data['intents'] if item['tag'] == intent[0])
-    return [intent[0], np.random.choice(responses)]
+# Split data (this example uses all data for training for simplicity)
+X_train, y_train = padded_sequences, categorical_labels
 
-text=""
-while text!="exit":
-    print()
-    text = str(input("query >> "))
-    output = get_response(text)
-    print("\n Predicted Tag: ", output[0])
-    print("\n bot advise >> ", output[1])
+# Define neural network architecture
+model = Sequential([
+    Embedding(input_dim=1000, output_dim=16, input_length=padded_sequences.shape[1]),
+    GlobalAveragePooling1D(),
+    Dense(24, activation='relu'),
+    Dense(len(label_encoder.classes_), activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+
+print("\nRecommended epoches: ", len(X_train), "\n")
+model.fit(X_train, y_train, epochs=150, verbose=1)
+
+model.save("model.keras")
+
+print("\nmodel saved in model.keras!")
